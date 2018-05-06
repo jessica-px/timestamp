7e7ee03be03b85859b88c67eb1ab3d538173d861
Jessica Peck <jessypeck@gmail.com>
2018-05-06 12:59:02 -0700

    Added test html

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 // Routes
 
 app.get("/", (req, res) => {
   res.render("index");
+})
+
+app.get("/test", (req, res) => {
+  res.render("test");
 })
 
 app.listen(port, () => {
