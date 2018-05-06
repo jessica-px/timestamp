1abed748070e3eff63a0fc01c06602b023907a86
Jessica Peck <jessypeck@gmail.com>
2018-05-06 12:31:52 -0700

    Init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname));
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.render("index");
+})
+
+app.listen(port, () => {
+  console.log("App Running...");
+});
