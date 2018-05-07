753917f0982471e52c851f244306da053b7d8526
Jessica Peck <jessypeck@gmail.com>
2018-05-06 17:40:07 -0700

    Added date/timestamp converter

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const moment = require('moment');
 
-app.use(express.static(__dirname + '/public'));
+moment().format();
+app.use(express.static('/public'));
 
 // Routes
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.sendFile(__dirname + "/public/index.html");
 })
 
-app.get("/test", (req, res) => {
-  res.render("test");
+app.get("/:time", (req, res) => {
+  const inputTime = req.params.time;
+  const time = formatTime(inputTime)
+  res.end(time);
 })
 
-app.listen(port, () => {
-  console.log("App Running...");
-});
+app.listen(port);
+
+
+// Format Time //
+
+const formatTime = (inputTime) => {
+  let time = {
+    date: null,
+    timestamp: null
+  }
+  // If input is unix timestamp
+  if (inputTime.match(/^\d*$/)){
+    time.date = moment.unix(parseInt(inputTime)).format('MM/DD/YYYY');
+    time.timestamp = inputTime;
+  }
+  // If input is human readable date (non-strict format)
+  else if(moment(inputTime).isValid()){
+    time.date = moment(inputTime).format('MM/DD/YYYY');
+    time.timestamp = moment(inputTime).unix();
+  }
+
+  return JSON.stringify(time);
+}
